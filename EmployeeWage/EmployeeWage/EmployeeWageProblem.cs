508af102df4c6721d93dbc5734a5e658c5685841
Pranav Waghmare <piyushw55@gmail.com>
2023-04-21 08:49:20 +0530

    [Pranav]Added: Ability to performe attendence

---
+++ EmployeeWage/EmployeeWage/EmployeeWageProblem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmployeeWageProblem
+    {
+        int empCheck;
+
+        public EmployeeWageProblem(int empChk)
+        {
+            this.empCheck = empChk;
+        }
+
+        public void Attendencs()
+        {
+            int IS_PRESENT = 1;
+            if (this.empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Wage Problems");
+
+            Random random= new Random();
+            
+            int emp = random.Next(0,2);
+
+
+            EmployeeWageProblem ewp = new EmployeeWageProblem(emp);
+
+            ewp.Attendencs();
+
+
+
+        }
+    }
+}
