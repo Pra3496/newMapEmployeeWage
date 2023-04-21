fccaceaeef82889c37e97865d825988a00b4b8a0
Pranav Waghmare <piyushw55@gmail.com>
2023-04-21 22:04:15 +0530

    [Pranav]Added: Employee Wage For a condition of Total working hours or days is Reached for month

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Employee Wage For a Month");
+            Console.WriteLine("Employee Wage For a condition of Total working hours or days is Reached for month");
             EmployeeWageProblem wageProblem = new EmployeeWageProblem();
             wageProblem.EmpWage();
 
