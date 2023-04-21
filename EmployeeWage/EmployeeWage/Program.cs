74df1ca98af65f5a8dfc8db9c51405bee3663c5b
Pranav Waghmare <piyushw55@gmail.com>
2023-04-21 21:50:53 +0530

    [Pranav]Added: Refactor the Code Using Switch Case Pattern

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Employee Wage Using Switch Case");
             EmployeeWageProblem wageProblem = new EmployeeWageProblem();
             wageProblem.EmpWage();
 
